c370d09b01395706890351a153dc78c8834b5b01
prolevelcode <quickhitdeal@gmail.com>
2025-08-09 16:35:18 +0530

    Delete client directory

--- client/src/lib/whatsapp.ts
+++
@@ -1,5 +0,0 @@
-export const shareOnWhatsApp = (message: string) => {
-  const encodedMessage = encodeURIComponent(message);
-  const whatsappUrl = `https://wa.me/?text=${encodedMessage}`;
-  window.open(whatsappUrl, '_blank');
-};
