bd705e3b2fdf94f4ceb43a79d6195b41c9380901
prolevelcode <quickhitdeal@gmail.com>
2025-08-09 16:11:56 +0530

    Update input-otp.tsx

--- client/src/components/ui/input-otp.tsx
+++ client/src/components/ui/input-otp.tsx
@@ -47,8 +47,8 @@ const InputOTPSlot = React.forwardRef<
     >
       {char}
       {hasFakeCaret && (
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000 pointer-events-none" />
         </div>
       )}
     </div>
